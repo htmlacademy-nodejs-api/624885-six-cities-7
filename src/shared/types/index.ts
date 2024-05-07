c177080ab6f53af2e3eb679ab8e6e9e5effcc52c
ivnikonHA <ivnikon@mail.ru>
2024-05-07 17:37:03 +0300

    Добавляет тип MockServerDataType

---
+++ src/shared/types/index.ts
@@ -0,0 +1,7 @@
+export { CategoryType } from './category.type.js';
+export { CityType } from './city.type.js';
+export { GoodsType } from './goods.type.js';
+export { LocationType } from './location.type.js';
+export { MockServerDataType } from './mock-server-data.type.js';
+export { OfferType } from './offer.type.js';
+export { UserType } from './user.type.js';
