7d49a9a277f3dd64714f93c28095c33df739c8a3
ivnikonHA <ivnikon@mail.ru>
2024-05-15 22:49:50 +0300

    Добавляет поля _id, id в UserModel

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -2,6 +2,9 @@ import { defaultClasses ,getModelForClass, prop } from '@typegoose/typegoose';
 
 import { UserCategoryType, UserType } from '../../types/index.js';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements UserType {
   @prop({ unique: true, required: true })
   public email: string;
