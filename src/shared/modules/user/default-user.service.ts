dad2fb294c75ec66a3d9737faf09766cf88e63f3
ivnikonHA <ivnikon@mail.ru>
2024-05-17 11:28:21 +0300

    Добавит внедрение модели в зависимости.

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,14 +1,25 @@
-import { DocumentType } from '@typegoose/typegoose';
+import { DocumentType, types } from '@typegoose/typegoose';
+import { inject } from 'inversify';
 
+import { Logger } from '../../libs/logger/logger.interface.js';
+import { Component } from '../../types/index.js';
 import { CreateUserDTO } from './dto/create-user.dto.js';
-import { UserEntity, UserModel } from './user.entity.js';
+import { UserEntity } from './user.entity.js';
 import { UserService } from './user-service.interface.js';
 
 export class DefaultUserService implements UserService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>
+  ) {}
+
   public async create(dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    return UserModel.create(user);
+    const result = await this.userModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+
+    return result;
   }
 }
--- src/shared/modules/user/user.container.ts
+++ src/shared/modules/user/user.container.ts
@@ -1,12 +1,15 @@
+import { types } from '@typegoose/typegoose';
 import { Container } from 'inversify';
 
 import { Component } from '../../types/index.js';
-import { DefaultUserService } from './index.js';
+import { DefaultUserService, UserEntity, UserModel } from './index.js';
 import { UserService } from './user-service.interface.js';
 
 export function createUserContainer() {
   const userContainer = new Container();
+
   userContainer.bind<UserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
+  userContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
   return userContainer;
 }
