38fd2de48c5be8705ca3c285abe46d19b48801f8
ivnikonHA <ivnikon@mail.ru>
2024-06-03 20:33:04 +0300

    Добавляет методы в UserService

--- src/shared/modules/user/user-service.interface.ts
+++ src/shared/modules/user/user-service.interface.ts
@@ -1,5 +1,6 @@
 import { DocumentType } from '@typegoose/typegoose';
 
+import { AddFavoriteDto } from './dto/add-favorite.dto.js';
 import { CreateUserDTO } from './dto/create-user.dto.js';
 import { UserEntity } from './user.entity.js';
 
@@ -7,4 +8,5 @@ export interface UserService {
   create(dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>>;
   findByEmail(email: string): Promise<DocumentType<UserEntity> | null>;
   findOrCreate(dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity> | null>;
+  addToFavorites(userId: string, dto: AddFavoriteDto): Promise<DocumentType<UserEntity> | null>;
 }
