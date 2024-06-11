e22e5cdca7b36b262c74f65cbcf35a68449c74f8
ivnikonHA <ivnikon@mail.ru>
2024-06-11 19:57:57 +0300

    Обновляет типы в OfferController

--- src/shared/modules/offer/offer-controller.ts
+++ src/shared/modules/offer/offer-controller.ts
@@ -6,7 +6,7 @@ import { fillDTO } from '../../helpers/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { BaseController, HttpError } from '../../libs/rest/index.js';
 import { Component, HttpMethod } from '../../types/index.js';
-import { DEFAULT_OFFER_COUNT, DetailOffersRdo, OfferService, OffersRdo, ParamOfferId } from './index.js';
+import { CreateOfferDTO, DEFAULT_OFFER_COUNT, DetailOffersRdo, OfferService, OffersRdo, ParamOfferId, UpdateOfferDto } from './index.js';
 
 @injectable()
 export class OfferController extends BaseController {
@@ -30,9 +30,8 @@ export class OfferController extends BaseController {
     this.ok(res, fillDTO(OffersRdo, offers));
   }
 
-  public async create(req: Request, res: Response): Promise<void> {
-    const newOffer = req.body;
-    const result = await this.offerService.create(newOffer);
+  public async create({ body }: Request<unknown, unknown, CreateOfferDTO>, res: Response): Promise<void> {
+    const result = await this.offerService.create(body);
 
     this.created(res, fillDTO(OffersRdo, result));
   }
@@ -40,28 +39,26 @@ export class OfferController extends BaseController {
   public async details({ params }: Request<ParamOfferId>, res: Response): Promise<void> {
     const result = await this.offerService.findById(params.id);
     if(!result) {
+      throw new HttpError(StatusCodes.NOT_FOUND, 'Offer not found', 'OfferController');
+    }
+    this.ok(res, fillDTO(DetailOffersRdo, result));
+  }
+
+  public async update({ body, params}: Request<ParamOfferId, unknown, UpdateOfferDto>, res: Response): Promise<void> {
+    const result = await this.offerService.updateById(params.id, body);
+
+    if(!result) {
       throw new HttpError(StatusCodes.NOT_FOUND, 'Offer not found');
     }
     this.ok(res, fillDTO(DetailOffersRdo, result));
   }
 
-  public async update(req: Request<ParamOfferId>, res: Response): Promise<void> {
-    const offer = await this.offerService.findById(req.params.id);
-    if(!offer) {
-      throw new HttpError(StatusCodes.NOT_FOUND, 'Offer not found');
-    }
-    const updateOffer = req.body;
-    const result = await this.offerService.updateById(offer.id, updateOffer);
-    this.ok(res, fillDTO(DetailOffersRdo, result));
-  }
-
-  public async delete({ params }: Request, res: Response): Promise<void> {
-    const offer = await this.offerService.findById(params.id);
-    if(!offer) {
-      throw new HttpError(StatusCodes.NOT_FOUND, 'Offer not found');
+  public async delete({ params }: Request<ParamOfferId>, res: Response): Promise<void> {
+    const result = await this.offerService.deleteById(params.id);
+    if(!result) {
+      throw new HttpError(StatusCodes.NOT_FOUND, 'Offer not found', 'OfferController');
     }
 
-    const result = await this.offerService.deleteById(offer.id);
     this.noContent(res, result);
   }
 }
