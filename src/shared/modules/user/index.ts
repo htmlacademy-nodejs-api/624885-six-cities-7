35c0a21aabb22b9fd21c400b9c61fa894fc2dfdc
ivnikonHA <ivnikon@mail.ru>
2024-06-15 22:45:23 +0300

    Добавит AuthService

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,5 +1,6 @@
 export { DefaultUserService } from './default-user.service.js';
 export { CreateUserDTO } from './dto/create-user.dto.js';
+export { LoginUserDto } from './dto/login-user.dto.js';
 export { createUserContainer } from './user.container.js';
 export { UserEntity, UserModel } from './user.entity.js';
 export { UserService } from './user-service.interface.js';
