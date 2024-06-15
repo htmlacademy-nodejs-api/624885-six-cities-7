5a23f9a05088a27e3066f3048cb9c98fd34aec0e
ivnikonHA <ivnikon@mail.ru>
2024-06-15 19:38:52 +0300

    Добавляет загрузку аватара

--- src/shared/modules/user/user-controller.ts
+++ src/shared/modules/user/user-controller.ts
@@ -3,8 +3,9 @@ import { StatusCodes } from 'http-status-codes';
 import { inject, injectable } from 'inversify';
 
 import { fillDTO } from '../../helpers/common.js';
+import { Config, RestSchema } from '../../libs/config/index.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
-import { BaseController, HttpError, ValidateDtoMiddleware } from '../../libs/rest/index.js';
+import { BaseController, HttpError, UploadFileMiddleware, ValidateDtoMiddleware, ValidateObjectIdMiddleware } from '../../libs/rest/index.js';
 import { Component } from '../../types/component.enum.js';
 import { HttpMethod } from '../../types/http-method.enum.js';
 import { CreateUserDTO } from './dto/create-user.dto.js';
@@ -16,6 +17,7 @@ import { UserService } from './user-service.interface.js';
 export class UserController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.UserService) private readonly userService: UserService
   ){
     super(logger);
@@ -32,6 +34,15 @@ export class UserController extends BaseController {
       method: HttpMethod.Post,
       handler: this.login,
       middlewares: [new ValidateDtoMiddleware(LoginUserDto)]
+    });
+    this.addRoute({
+      path: '/:userId/avatar',
+      method: HttpMethod.Post,
+      handler: this.uploadAvatar,
+      middlewares: [
+        new ValidateObjectIdMiddleware('userId'),
+        new UploadFileMiddleware(this.config.get('UPLOAD_DIRECTORY'), 'avatar')
+      ]
     });
   }
 
@@ -64,5 +75,11 @@ export class UserController extends BaseController {
       'Not implemented yet',
       'UserController'
     );
+  }
+
+  public async uploadAvatar(req: Request, res: Response) {
+    this.created(res, {
+      filepath: req.file?.path
+    });
   }
 }
