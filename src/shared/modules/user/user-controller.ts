117dcab2f0fc4ed863187773fa18ac63c91e395e
ivnikonHA <ivnikon@mail.ru>
2024-06-15 23:19:40 +0300

    Реализует метод login

--- src/shared/modules/user/user-controller.ts
+++ src/shared/modules/user/user-controller.ts
@@ -8,8 +8,10 @@ import { Logger } from '../../libs/logger/logger.interface.js';
 import { BaseController, HttpError, UploadFileMiddleware, ValidateDtoMiddleware, ValidateObjectIdMiddleware } from '../../libs/rest/index.js';
 import { Component } from '../../types/component.enum.js';
 import { HttpMethod } from '../../types/http-method.enum.js';
+import { AuthService } from '../auth/auth-service.interface.js';
 import { CreateUserDTO } from './dto/create-user.dto.js';
 import { LoginUserDto } from './dto/login-user.dto.js';
+import { LoggedUserRdo } from './rdo/logged-user.rdo.js';
 import { UserRdo } from './rdo/user.rdo.js';
 import { UserService } from './user-service.interface.js';
 
@@ -18,7 +20,8 @@ export class UserController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
-    @inject(Component.UserService) private readonly userService: UserService
+    @inject(Component.UserService) private readonly userService: UserService,
+    @inject(Component.AuthService) private readonly authService: AuthService
   ){
     super(logger);
 
@@ -60,21 +63,16 @@ export class UserController extends BaseController {
     this.created(res, fillDTO(UserRdo, result));
   }
 
-  public async login(req: Request, _res: Response): Promise<void> {
+  public async login(req: Request, res: Response): Promise<void> {
     const newUser = req.body;
-    const existUser = await this.userService.findByEmail(newUser.email);
-    if(!existUser) {
-      throw new HttpError(
-        StatusCodes.UNAUTHORIZED,
-        `User with email ${newUser.email} not found.`,
-        'UserController'
-      );
-    }
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      'Not implemented yet',
-      'UserController'
-    );
+    const user = await this.authService.verify(newUser);
+    const token = await this.authService.authenticate(user);
+    const responceData = fillDTO(LoggedUserRdo, {
+      email: user.email,
+      token
+    });
+
+    this.ok(res, responceData);
   }
 
   public async uploadAvatar(req: Request, res: Response) {
